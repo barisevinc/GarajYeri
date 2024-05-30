737201399ebbd43c1571a5c248763ee95e1fc409
Barış Sevinç <barissevinc088@gmail.com>
2024-05-30 22:39:31 +0300

    AppUser Class'ı oluşturuldu.

--- GarajYeri.Models/AppUser.cs
+++ GarajYeri.Models/AppUser.cs
@@ -11,5 +11,6 @@ namespace GarajYeri.Models
         public string FullName {  get; set; }
         public string UserName {  get; set; }
         public string Password { get; set; }
+        public bool IsAdmin { get; set; } = false;
     }
 }
