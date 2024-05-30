c2678cec5e68ac7390bc9e69e08cc598dc5a8d28
Barış Sevinç <barissevinc088@gmail.com>
2024-05-31 01:06:53 +0300

    ApplicationDbContext oluşturuldu

--- GarajYeri.Data/ApplicationDbContext.cs
+++ GarajYeri.Data/ApplicationDbContext.cs
@@ -29,5 +29,11 @@ namespace GarajYeri.Data
         public virtual DbSet<VehicleProcessType> VehicleProcessTypes {  get; set; }
         public virtual DbSet<VehicleType> VehicleTypes {  get; set; }
 
+
+        //FLUENT API
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Vehicle>().Property(p => p.Name).HasMaxLength(50);
+        //}
     }
 }
