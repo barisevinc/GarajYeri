496f111c669c7a0380779fe96d2a3018897c7341
Barış Sevinç <barissevinc088@gmail.com>
2024-05-30 22:53:45 +0300

    Vehicle Class'ı oluşturuldu.

---
+++ GarajYeri.Models/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarajYeri.Models
+{
+    public class Vehicle:BaseModel
+    {
+        public string LicensePlate {  get; set; }
+        public string Name { get; set; }
+        public double Odometer {  get; set; }
+
+    }
+}
