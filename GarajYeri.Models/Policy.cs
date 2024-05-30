5ef9f5512fa6da20c2830e07a3df81a9b49b632c
Barış Sevinç <barissevinc088@gmail.com>
2024-05-30 23:59:17 +0300

    Policy modeli eklendi

---
+++ GarajYeri.Models/Policy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarajYeri.Models
+{
+    public class Policy:BaseModel
+    {
+        public string Name {  get; set; }
+        public string CompanyName {  get; set; }
+        public string Validity {  get; set; }
+    }
+}
