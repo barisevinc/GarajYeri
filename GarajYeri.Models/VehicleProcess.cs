c8ba744671ab23e8c365c31ec1373665459475fa
Barış Sevinç <barissevinc088@gmail.com>
2024-05-30 23:44:46 +0300

    VehicleProcess modeli oluşturuldu.

---
+++ GarajYeri.Models/VehicleProcess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarajYeri.Models
+{
+    public class VehicleProcess:BaseModel
+    {
+        public  double Odemeter {  get; set; }
+        public double? Price { get; set; }
+    }
+}
